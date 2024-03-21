fbad6700bf4dada949df16ff28ce7af41c061598
Troy <5659019+troyhacks@users.noreply.github.com>
2024-03-21 15:18:46 -0400

    Tidy

--- src/main.cpp
+++ src/main.cpp
@@ -6,61 +6,32 @@
 
 Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
     5 /* DE */, 3 /* VSYNC */, 46 /* HSYNC */, 7 /* PCLK */,
-    1 /* R0 */, 2 /* R1 */, 42 /* R2 */, 41 /* R3 */, 40 /* R4 */,
-    39 /* G0 */, 0 /* G1 */, 45 /* G2 */, 48 /* G3 */, 47 /* G4 */, 21 /* G5 */,
-    14 /* B0 */, 38 /* B1 */, 18 /* B2 */, 17 /* B3 */, 10 /* B4 */,
+    1 /* R0 */, 2 /* R1 */, 42 /* R2 */, 41 /* R3 */, 40 /* R4 */,                // if not R0, start at lowest R
+    39 /* G0 */, 0 /* G1 */, 45 /* G2 */, 48 /* G3 */, 47 /* G4 */, 21 /* G5 */,  // if not G0, start at lowest G
+    14 /* B0 */, 38 /* B1 */, 18 /* B2 */, 17 /* B3 */, 10 /* B4 */,              // if not B0, start at lowest B
     0 /* hsync_polarity */, 210 /* hsync_front_porch */, 30 /* hsync_pulse_width */, 16 /* hsync_back_porch */,
     0 /* vsync_polarity */, 22 /* vsync_front_porch */, 13 /* vsync_pulse_width */, 10 /* vsync_back_porch */,
     1 /* pclk_active_neg */, 16000000 /* prefer_speed */);
+
 Arduino_RGB_Display *gfx = new Arduino_RGB_Display(800, 480, rgbpanel);
 
 CRGB leds[1];
 
-/* First, lets define the hardware pins that we are using with our ESP32. We
-  need to define which pin is transmitting data and which pin is receiving data.
-  DMX circuits also often need to be told when we are transmitting and when we
-  are receiving data. We can do this by defining an enable pin. */
 int transmitPin1 = 16;
 int receivePin1 = 15;
 int enablePin1 = -1;
 
 int transmitPin2 = 6;
-int receivePin2 = 19;
+int receivePin2 = -1;
 int enablePin2 = -1;
 
-/* Make sure to double-check that these pins are compatible with your ESP32!
-  Some ESP32s, such as the ESP32-WROVER series, do not allow you to read or
-  write data on pins 16 or 17, so it's always good to read the manuals. */
-
-/* Next, lets decide which DMX port to use. The ESP32 has either 2 or 3 ports.
-  Port 0 is typically used to transmit serial data back to your Serial Monitor,
-  so we shouldn't use that port. Lets use port 1! */
 dmx_port_t dmxPort1 = 1;
 dmx_port_t dmxPort2 = 2;
 
-/* Now we want somewhere to store our DMX data. Since a single packet of DMX
-  data can be up to 513 bytes long, we want our array to be at least that long.
-  This library knows that the max DMX packet size is 513, so we can fill in the
-  array size with `DMX_PACKET_SIZE`. */
 byte data[DMX_PACKET_SIZE];
 
-/* The last two variables will allow us to know if DMX has been connected and
-  also to update our packet and print to the Serial Monitor at regular
-  intervals. */
 bool dmxIsConnected = false;
 unsigned long lastUpdate = millis();
-
-// Extend IO Pin define
-#define TP_RST 1
-#define LCD_BL 2
-#define LCD_RST 3
-#define SD_CS 4
-#define USB_SEL 5
-
-// I2C Pin define
-#define I2C_MASTER_NUM 0
-#define I2C_MASTER_SDA_IO 8
-#define I2C_MASTER_SCL_IO 9
 
 uint16_t w, h;
 
@@ -68,8 +39,6 @@ void setup() {
 
   Serial.begin(115200); /* prepare for possible serial debug */
   delay(100);
-
-  Serial.printf("*** SOC has %d UARTs\n",SOC_UART_NUM);
   
   dmx_config_t config = DMX_CONFIG_DEFAULT;
 
@@ -156,11 +125,11 @@ void loop() {
       dmx_wait_sent(dmxPort2, DMX_TIMEOUT_TICK);
 
       if (now - lastUpdate > 200) {
+
         gfx->setCursor(0, 10);
         gfx->println(" TroyHacks DMX Toy");
         gfx->setCursor(0, gfx->getCursorY()+15);
 
-        // gfx->fillScreen(BLACK);
         for (int i=1; i<=35; i++) {
           if (i%5 == 0) {
             gfx->printf("%03u",data[i]);
@@ -169,18 +138,25 @@ void loop() {
           }
           Serial.printf("%03u ",data[i]);
         }
+
         Serial.println();
         // gfx->flush();
         lastUpdate = now;
+
       }
+
     } else {
+
       gfx->fillScreen(BLACK);
       gfx->setCursor(0, 10);
       gfx->println(" TroyHacks DMX Toy");
       gfx->setCursor(0, gfx->getCursorY()+15);
       gfx->println("DMX Error!");
+
       Serial.println("A DMX error occurred.");
+
     }
+
   } else if (dmxIsConnected) {
     
     Serial.println("DMX was disconnected.");
@@ -194,4 +170,5 @@ void loop() {
     dmxIsConnected = false;
 
   }
+
 }
